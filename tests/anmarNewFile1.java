08b9489484d510f0de94cdf749a05533dbcc4e79
samarqa1 <samarqa@uwindsor.ca>
2025-12-09 15:05:35 -0500

    anmarNewFile1.java

--- tests/anmarNewFile1.java
+++ tests/anmarNewFile1.java
@@ -1,11 +1,19 @@
 class CalculatorNew {
 
+    // Constructor
+    public CalculatorNew() {
+        // Initialization
+    }
+
+    // Addition method
     public int add(int a, int b) {
         return a + b;
     }
 
+    // Subtraction method
     public int subtract(int a, int b) {
-        return a - b;
+        int difference = a - b;
+        return difference;
     }
 
     public int multiply(int a, int b) {
@@ -13,7 +21,18 @@ class CalculatorNew {
     }
 
     public int divide(int a, int b) {
-        if (b == 0) throw new IllegalArgumentException("Division by zero");
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
         return a / b;
     }
+
+    public int modulus(int a, int b) {
+        return a % b;
+    }
+
+    public void printResult(int result) {
+        System.out.println("Result: " + result);
+    }
+
 }
