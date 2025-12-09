15e86087e4291d8382aa2edef44ffc185c89a8cf
samarqa1 <samarqa@uwindsor.ca>
2025-12-09 15:07:03 -0500

    Update anmarOldFile1.java

--- tests/anmarOldFile1.java
+++ tests/anmarOldFile1.java
@@ -1,7 +1,8 @@
 class CalculatorOld {
 
     public int add(int a, int b) {
-        return a + b;
+        int result = a + b;
+        return result;
     }
 
     public int subtract(int a, int b) {
@@ -9,6 +10,12 @@ class CalculatorOld {
     }
 
     public int multiply(int a, int b) {
-        return a * b;
+        int product = a * b;
+        return product;
     }
+
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
 }
