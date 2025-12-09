52591c4f5521bfcd8a76f876941e18188256974c
samarqa1 <samarqa@uwindsor.ca>
2025-12-09 15:13:55 -0500

    Update anmarOldFile3.java

--- tests/anmarOldFile3.java
+++ tests/anmarOldFile3.java
@@ -1,18 +1,27 @@
 class RectangleOld {
-
+    // Dimensions
     public int width;
     public int height;
 
+    // Constructor
     public RectangleOld(int width, int height) {
         this.width = width;
         this.height = height;
     }
 
-    public int area() {
-        return width * height;
+    // Calculate area
+    public int calculateArea() {
+        int area = width * height;
+        return area;
     }
 
-    public int perimeter() {
+    // Calculate perimeter
+    public int calculatePerimeter() {
         return 2 * (width + height);
+    }
+
+    // Display info
+    public void display() {
+        System.out.println("Width: " + width + ", Height: " + height);
     }
 }
