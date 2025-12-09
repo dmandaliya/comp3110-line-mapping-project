6dcd3a06917271496bb9ccb16d60938eb4c2835d
samarqa1 <samarqa@uwindsor.ca>
2025-12-09 15:14:29 -0500

    Update anmarNewFile3.java

--- tests/anmarNewFile3.java
+++ tests/anmarNewFile3.java
@@ -1,22 +1,41 @@
 class RectangleNew {
+    // Properties
+    private int width;
+    private int height;
 
-    public int width;
-    public int height;
-
+    // validatiion
     public RectangleNew(int width, int height) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Dimensions must be positive");
+        }
         this.width = width;
         this.height = height;
     }
 
+    // Get area
     public int area() {
         return width * height;
     }
 
+    // Get perimeter
     public int perimeter() {
-        return 2 * (width + height);
+        int result = 2 * (width + height);
+        return result;
     }
 
+    // Check if square
     public boolean isSquare() {
         return width == height;
+    }
+
+    // Get diagonal length
+    public double diagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
+
+    // To string representation
+    @Override
+    public String toString() {
+        return "Rectangle[width=" + width + ", height=" + height + "]";
     }
 }
